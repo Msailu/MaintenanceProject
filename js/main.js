8812864fd77812362c84017f43fb8c6f91211fba
Sailaja <sailaja.ycs@gmail.com>
2021-02-19 14:02:08 +1100

    Added code to send request using Axios

--- js/main.js
+++ js/main.js
@@ -1,45 +1,97 @@
+
+//const axios = require('axios');
 //Code to fill the catagory select box 
 
-//let status=(form.querySelector('select[name="status"]')).value;
-const income= ['Maintenance','water','Settle Court Rent','FD Interest Amount'];
-const expenditure=['Security Salary','Gardener Salary','Admin Salary','Panjayathu Maintenance','Electricity Board','Infrastructure','Regular Maintenance'];
-let revenueType=document.getElementById('revenueType');
-let catagory = document.getElementById('catagory');
-revenueType.addEventListener('change', function() {
+const income= ['Choose catagory..','Maintenance','water','Settle Court Rent','FD Interest Amount'];
+const expenditure=['Choose catagory..','Security Salary','Gardener Salary','Admin Salary','Panjayathu Maintenance','Electricity Board','Infrastructure','Regular Maintenance'];
+let catagoryType=document.getElementById('catagory');
+let subCatagory = document.getElementById('subCatagory');
+catagoryType.addEventListener('change', function() {
     //This line removes the previosly added options on change event
-    document.querySelectorAll('#catagory option').forEach(option => option.remove());
    
-    if (revenueType.value === 'I') {
+     document.querySelectorAll('#subCatagory option').forEach(option => option.remove());
+   
+    if (catagoryType.value === 'I') {
         
         for(i=0;i<income.length;i++){
             let opt = document.createElement('option');
             opt.value = income[i];
             opt.innerHTML = income[i];
-            catagory.appendChild(opt);    
+            subCatagory.appendChild(opt);    
         }// end for loop
         
      }
-     else { //revenueType.value === 'E'     expenditure
+     else { //catagoryType.value === 'E'     expenditure
         for(i=0;i<expenditure.length;i++){
             let opt = document.createElement('option');
             opt.value = expenditure[i];
             opt.innerHTML = expenditure[i];
-            catagory.appendChild(opt); 
+            subCatagory.appendChild(opt); 
         }          
      }
 })
 
+
+  $("#submitBtn").on("click", () => {    
+   
+    console.log('post request');
+    
+   let date=$('#date').val();
+   let category =$('#catagory').val();
+   let subcategory=$('#subCatagory').val();
+   let amount =$('#amount').val();
+
+   let data = {
+    date ,
+    category ,
+    subcategory ,
+    amount
+  };
+
+   let axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
+    }
+  };
+
+  
+axios
+    .post('http://www.example.org/inc-exp-record',data, axiosConfig)
+    .then(res => console.log(res))
+    .catch(err => console.error(req));
+ 
+
+}); // end of submit button on click event
+
+
+// INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(
+    config => {
+      console.log(
+        `${config.method.toUpperCase()} request sent to ${
+          config.url
+        } at ${new Date().getTime()}`
+      );
+  
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+
 $(document).ready(function(){
     var date_input=$('input[name="date"]'); //our date input has the name "date"
-  
+    
     date_input.datepicker({
         format: 'mm/dd/yyyy',
-      
         todayHighlight: true,
         autoclose: true,
         orientation:  "top auto"
 
-    })
+    });
+   
 });
 
  
